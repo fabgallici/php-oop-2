c546e59d3b2dd905644d4ef3c17b0ffaa24c89ba
cabal82 <fabriziogallici@gmail.com>
2020-01-09 14:26:08 +0100

    class persona

---
+++ customer.php
@@ -0,0 +1,18 @@
+<?php
+// - Definire la classe Persona caratterizzata da nome e cognome
+class Persona {
+  public $name;
+  public $surname;
+  public function __constructor($name, $surname) {
+      $this->name = $name;
+      $this->surname = $surname;
+  }
+  public function __toString()
+  {
+    return "name: " . $this->name . " - surname: " . $this->surname;
+  }
+}
+// - Definire la classe Ospite (che eredita da Persona) caratterizzata da nome, cognome e anno di nascita
+// - Definire la classe Pagante (che eredita da Persona) caratterizzata da nome, cognome e indirizzo (di residenza)
+// - Per ogni classe definire costruttore e toString in maniera appropriata, eventualmente richiamando i metodi della classe padre
+// - Eseguire dei test, istanziando ogni classe definita e testando la correttezza dei risultati attesi
