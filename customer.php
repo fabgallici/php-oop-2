699f90ac2d792b2eb5e8af9d8da17f2f85518c93
cabal82 <fabriziogallici@gmail.com>
2020-01-09 14:38:26 +0100

    class ospite + test

--- customer.php
+++ customer.php
@@ -13,8 +13,21 @@ class Persona {
   }
 }
 // - Definire la classe Ospite (che eredita da Persona) caratterizzata da nome, cognome e anno di nascita
+class Ospite extends Persona {
+  public $yearOfBirth;
+  public function __construct($name, $surname, $yearOfBirth) {
+    $this->yearOfBirth = $yearOfBirth;
+    parent::__construct($name, $surname);
+  }
+  public function __toString() {
+    return parent::__toString() . " - year of birth: " . $this->yearOfBirth;
+  }
+}
 // - Definire la classe Pagante (che eredita da Persona) caratterizzata da nome, cognome e indirizzo (di residenza)
 // - Per ogni classe definire costruttore e toString in maniera appropriata, eventualmente richiamando i metodi della classe padre
 // - Eseguire dei test, istanziando ogni classe definita e testando la correttezza dei risultati attesi
 $utente1 = new Persona('Gianni', 'Rossi');
-echo $utente1;
+echo $utente1 . "<br>";
+
+$utente2 = new Ospite('Alberto', 'Bianchi', 1970);
+echo $utente2 . "<br>";
