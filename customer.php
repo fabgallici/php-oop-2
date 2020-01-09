1bb2e40a7113f613af3b0620a1146516d17e6a27
cabal82 <fabriziogallici@gmail.com>
2020-01-09 14:44:33 +0100

    pagante + test

--- customer.php
+++ customer.php
@@ -1,8 +1,8 @@
 <?php
 // - Definire la classe Persona caratterizzata da nome e cognome
 class Persona {
-  public $name;
-  public $surname;
+  private $name;
+  private $surname;
   public function __construct($name, $surname) {
       $this->name = $name;
       $this->surname = $surname;
@@ -14,7 +14,7 @@ class Persona {
 }
 // - Definire la classe Ospite (che eredita da Persona) caratterizzata da nome, cognome e anno di nascita
 class Ospite extends Persona {
-  public $yearOfBirth;
+  private $yearOfBirth;
   public function __construct($name, $surname, $yearOfBirth) {
     $this->yearOfBirth = $yearOfBirth;
     parent::__construct($name, $surname);
@@ -24,6 +24,17 @@ class Ospite extends Persona {
   }
 }
 // - Definire la classe Pagante (che eredita da Persona) caratterizzata da nome, cognome e indirizzo (di residenza)
+class Pagante extends Persona {
+  private $address;
+  public function __construct($name, $surname, $address) {
+    $this->address = $address;
+    parent::__construct($name, $surname);
+  }
+  public function __toString()
+  {
+    return parent::__toString() . " - address: " . $this->address;
+  }
+}
 // - Per ogni classe definire costruttore e toString in maniera appropriata, eventualmente richiamando i metodi della classe padre
 // - Eseguire dei test, istanziando ogni classe definita e testando la correttezza dei risultati attesi
 $utente1 = new Persona('Gianni', 'Rossi');
@@ -31,3 +42,6 @@ echo $utente1 . "<br>";
 
 $utente2 = new Ospite('Alberto', 'Bianchi', 1970);
 echo $utente2 . "<br>";
+
+$utente3 = new Pagante('Pinco', 'Pallino', 'via Roma');
+echo $utente3;
